c6062069e19db57754f51e0af50df08348b81981
Orlando Marques <realorlanditomadrid@gmail.com>
2022-01-08 22:07:22 +0100

    ADD: products endpoint

--- routes/api.js
+++ routes/api.js
@@ -6,4 +6,25 @@ router.get('/', function(req, res, next) {
     res.send('api ready');
 });
 
+router.post('/products',function (req,res,next) {
+   res.json({
+       ...req.body,
+       "fulfillmentMessages": [
+           {
+               "card": {
+                   "title": "card title",
+                   "subtitle": "card text",
+                   "imageUri": "https://example.com/images/example.png",
+                   "buttons": [
+                       {
+                           "text": "button text",
+                           "postback": "https://example.com/path/for/end-user/to/follow"
+                       }
+                   ]
+               }
+           }
+       ]
+   });
+});
+
 module.exports = router;
