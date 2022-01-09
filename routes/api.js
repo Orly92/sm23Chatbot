c5ee8de3f78d66465faa7716a4b3b762dd7d4bd8
Orlando Marques <realorlanditomadrid@gmail.com>
2022-01-09 11:16:59 +0100

    ADD: /api get endpoint

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/',function (req,res,next) {
+    res.send('Api ready');
+})
+router.post('/', function(req, res, next) {
     res.json({
         ...req.body,
         "fulfillmentMessages": [
