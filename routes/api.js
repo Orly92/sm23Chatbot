2695572c80e4a4f335812ea7bcfd54b6dd3d529b
Orlando Marques <realorlanditomadrid@gmail.com>
2022-01-09 11:15:40 +0100

    ADD: / endpoint

--- routes/api.js
+++ routes/api.js
@@ -3,7 +3,24 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.send('api ready');
+    res.json({
+        ...req.body,
+        "fulfillmentMessages": [
+            {
+                "card": {
+                    "title": "card title",
+                    "subtitle": "card text",
+                    "imageUri": "https://example.com/images/example.png",
+                    "buttons": [
+                        {
+                            "text": "button text",
+                            "postback": "https://example.com/path/for/end-user/to/follow"
+                        }
+                    ]
+                }
+            }
+        ]
+    });
 });
 
 router.post('/products',function (req,res,next) {
