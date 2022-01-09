49dd75def603937e8584318494c7c670ed8d2431
Orlando Marques <realorlanditomadrid@gmail.com>
2022-01-09 16:43:42 +0100

    FIX: provinces services part 3

--- services/provincesServices.js
+++ services/provincesServices.js
@@ -8,11 +8,11 @@ const getProvincesResponse = (body)=>{
     const provincesOriente = ['Villa Clara','Cienfuegos','Sancti Spiritus','Ciego de Avila','Camaguey','Las Tunas',
     'Holguin','Granma','Santiago de Cuba','Guantanamo'];
     if(provincesOccidente.some((item)=> item.toLowerCase() === province.toLowerCase()))
-        response = 'De 5 a 7 días';
+        response = 'De 5 a 7 días, después de ser aprobada';
     if(provincesCapital.some((item)=> item.toLowerCase() === province.toLowerCase()))
-        response = 'De 3 a 5 días';
+        response = 'De 3 a 5 días, después de ser aprobada';
     if(provincesOriente.some((item)=> item.toLowerCase() === province.toLowerCase()))
-        response = 'De 10 a 15 días';
+        response = 'De 10 a 15 días, después de ser aprobada';
 
     return [{
         "text": {
